009ed641d4a14fcee821d6a2de3e8bf7ac950404
AgnesaS <44116298+AgnesaS@users.noreply.github.com>
2021-02-13 18:08:20 +0100

    Update WifiPassApp.py

--- WifiPassApp.py
+++ WifiPassApp.py
@@ -9,3 +9,28 @@ canvas = tk.Canvas(root, width=900, height=900)
 canvas.grid(columnspan=3, rowspan=3)
 pass_details = StringVar()
 myList = []
+
+def see_wifi_pass():
+    import subprocess
+    global myList
+    data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+    profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
+    myList.append("------------------------")
+    for i in profiles:
+        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
+        results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
+        try:
+            if not re.search('[A-Z]', results[0]) or not re.search('[a-z]', results[0]) or not re.search('[0-9]', results[0]):
+                myList.append("Wifi-->" + i)
+                # myList.append("--")
+                myList.append("Password-->" +results[0]+"| Weak")
+                myList.append("------------------------")
+            else:
+                myList.append("Wifi-->" + i)
+                # myList.append("--")
+                myList.append("Password-->" +results[0]+"| Strong")
+                myList.append("------------------------")
+        except IndexError:
+            myList.append("Wifi-->" +i)
+            # myList.append("--")
+            myList.append("")
