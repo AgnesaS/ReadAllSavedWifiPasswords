d5d69eb99d4fd4135225ba3281c62dc5f3c98ff0
diellzaaliu <58387674+diellzaaliu@users.noreply.github.com>
2021-02-13 23:07:54 +0100

    Update WifiPassApp.py

--- WifiPassApp.py
+++ WifiPassApp.py
@@ -8,29 +8,31 @@ import pandas as pd
 from pandas import Series, DataFrame
 import matplotlib.pyplot as plt
 
+#krijimi i root dritares 
 root = tk.Tk()
-root.geometry("900x900")
+root.geometry("900x900") #dimensionet e dritares
 pass_details = StringVar()
 myList = []
 strength= []
 data = []
 
-
+#Vendosja dhe pershtatja e nje logoje ne background
 bg = PhotoImage(file="logo.png")
 my_label=Label(root,image=bg)
 my_label.place(x=0,y=0 ,  relwidth=1, relheight=1)
 
+#funksioni per gjetjen e wifi-ve dhe fjalekalimeve perkatese
 def see_wifi_pass():
     import subprocess
     global myList
-    data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n')
+    data = subprocess.check_output(['netsh', 'wlan', 'show', 'profiles']).decode('utf-8').split('\n') #komanda per gjetjen e profileve(wifi-ve)
     profiles = [i.split(":")[1][1:-1] for i in data if "All User Profile" in i]
     myList.append("------------------------")
     for i in profiles:
-        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n')
+        results = subprocess.check_output(['netsh', 'wlan', 'show', 'profile', i, 'key=clear']).decode('utf-8').split('\n') #komanda per gjetjen e fjalekalimeve
         results = [b.split(":")[1][1:-1] for b in results if "Key Content" in b]
         try:
-            if not re.search('[A-Z]', results[0]) or not re.search('[a-z]', results[0]) or not re.search('[0-9]', results[0]):
+            if not re.search('[A-Z]', results[0]) or not re.search('[a-z]', results[0]) or not re.search('[0-9]', results[0]): #kondita per kriteret e kompleksitetit te fjalekalimit
                 myList.append("Wifi-->" + i)
                 # myList.append("--")
                 myList.append("Password-->" +results[0]+"| Weak")
@@ -45,39 +47,42 @@ def see_wifi_pass():
            
             myList.append("")
 
+#funksioni per shnderrimin ne str te variables myList e cila permbane ciftin wifi-fjalekalim
 def show_wifi_pass():
     def listToString(s):
-        # initialize an empty string
+        # inicializimi i nje stringu bosh
         myStr = ""
 
-        # traverse in the string
+        # iterimi neper string
         for ele in s:
             myStr = myStr +  ele + "\n"
 
-            # return string
+            # kthimi i stringut
         return myStr
     myStr = listToString(myList)
     pass_details.set(myStr)
 
-
+#funksioni per kopjimin e fjalekalimeve
 def copytoclipboard():
     password = pass_details.get()
     pyperclip.copy(password)
-    
+
+#funksioni per shnderrimin ne str te te variables strength e cila permbane kompleksitetin e fjalekalimeve
 def show_strength():
     def listToString(s):
-  # initialize an empty string
+       # inicializimi i nje stringu bosh
         mystr = ""
 
-  # traverse in the string
+      # iterimi neper string
         for ele in s:
             mystr = mystr +  ele + "\n"
 
-       # return string
+       # kthimi i stringut
         return mystr
     mystr = listToString(strength)
     strength_details.set(mystr)
-    
+
+#funksioni per krijimin e grafit
 def graph():
     strength1 = strength_details.get()
     strength2= strength1.split()
